248c12ce640cbf7d5fcdceff9f9c9d16acc79033
Dmitry Kovanikov <kovanikov@gmail.com>
2017-04-04 21:36:10 +0300

    Add benchmarks for example README

--- bench/Main.hs
+++ bench/Main.hs
@@ -16,7 +16,8 @@ import qualified Data.Text.Format        as TF
 import           Data.Text.Format.Params as TF
 import qualified Data.Text.Lazy          as LT
 import           Fmt                     (( #| ), (|#))
-import           Formatting              (formatToString, int, sformat, (%))
+import           Formatting              (formatToString, int, sformat, stext, string,
+                                          (%))
 import           Text.Printf             (printf)
 
 import           Criterion               (Benchmark, bench, bgroup, nf)
@@ -77,6 +78,24 @@ main = defaultMain
       , taggedB "interpolate" $ \(a,b) -> [i|hello #{a} world #{b}|]
       , taggedB "show"        $ \(a,b) -> "hello " ++ show a ++ " world " ++ show b
       , taggedB "printf"      $ \(a,b) -> printf "hello %d world %d" a b
+      ]
+    ]
+  , bgroup "readme"
+    [ bTextGroup (9 :: Int, "Beijing" :: Text)
+      [ taggedB "fmt"         $ \(n,city) -> "There are "#|n|#" million bicycles in "#|city|#"."
+      , taggedB "formattting" $ \(n,city) -> sformat ("There are "%int%" million bicycles in "%stext%".") n city
+      , taggedB "text-format" $ format' "There are {} million bicycles in {}."
+      , taggedB "interpolate" $ \(n,city) -> T.pack [i|There are #{n} million bicycles in #{city}.|]
+      , taggedB "show"        $ \(n,city) -> "There are " <> tshow n <> " million bicycles in " <> city <> "."
+      , taggedB "printf"      $ \(n,city) -> T.pack $ printf "There are %d million bicycles in %s." n city
+      ]
+    , bStringGroup (9 :: Int, "Beijing" :: String)
+      [ taggedB "fmt"         $ \(n,city) -> "There are "#|n|#" million bicycles in "#|city|#"."
+      , taggedB "formattting" $ \(n,city) -> formatToString ("There are "%int%" million bicycles in "%string%".") n city
+      , taggedB "text-format" $ formatS "There are {} million bicycles in {}."
+      , taggedB "interpolate" $ \(n,city) -> [i|There are #{n} million bicycles in #{city}.|]
+      , taggedB "show"        $ \(n,city) -> "There are " ++ show n ++ " million bicycles in " ++ city ++ "."
+      , taggedB "printf"      $ \(n,city) -> printf "There are %d million bicycles in %s." n city
       ]
     ]
   ]
