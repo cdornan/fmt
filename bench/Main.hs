54b2ea16eae0f0df5ed0328d5917ba9130fdeab7
Artyom <yom@artyom.me>
2017-04-04 19:16:45 +0200

    Make benchmarks compile with older GHC + minor changes

--- bench/Main.hs
+++ bench/Main.hs
@@ -1,20 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes       #-}
 {-# LANGUAGE TemplateHaskell   #-}
-{-# LANGUAGE TypeApplications  #-}
 
 -- | Benchmarks for @fmt@ library.
 
 module Main where
 
-import           Control.DeepSeq         (NFData)
 import           Data.Monoid             ((<>))
-import           Data.String             (IsString)
 import           Data.String.Interpolate (i)
 import           Data.Text               (Text)
 import qualified Data.Text               as T
 import qualified Data.Text.Format        as TF
-import           Data.Text.Format.Params (Params)
+import           Data.Text.Format.Params as TF
 import qualified Data.Text.Lazy          as LT
 import           Fmt                     (( #| ), (|#))
 import           Formatting              (int, sformat, (%))
@@ -23,22 +20,31 @@ import           Text.Printf             (printf)
 import           Criterion               (Benchmarkable, bench, bgroup, nf)
 import           Criterion.Main          (defaultMain)
 
-simpleBench :: (NFData s, IsString s) => ((Int, Int) -> s) -> Benchmarkable
+simpleBench :: ((Int, Int) -> Text) -> Benchmarkable
 simpleBench fmt = nf fmt (1,2)
 
-format' :: Params ps => TF.Format -> ps -> Text
+format' :: TF.Params ps => TF.Format -> ps -> Text
 format' f = LT.toStrict . TF.format f
+
+tshow :: Show a => a -> Text
+tshow x = T.pack (show x)
 
 main :: IO ()
 main = defaultMain
   [ bgroup "simple"
     [ bgroup "text"
-      [ bench "fmt" $ simpleBench @Text (\(a,b) -> "hello "#|a|#" world "#|b|#"")
-      , bench "fng" $ simpleBench @Text (\(a,b) -> sformat ("hello "%int%" world "%int) a b)
-      , bench "shw" $ simpleBench @Text (\(a,b) -> "hello " <> T.pack (show a) <> " world " <> T.pack (show b))
-      , bench "tft" $ simpleBench @Text (format' "hello {} world {} ")
-      , bench "prf" $ simpleBench @Text (\(a,b) -> T.pack $ printf "hello %d world %d" a b)
-      , bench "int" $ simpleBench @Text (\(a,b) -> T.pack [i|hello #{a} world #{b}|])
+      [ bench "fmt" $ simpleBench
+            (\(a,b) -> "hello "#|a|#" world "#|b|#"")
+      , bench "formatting" $ simpleBench
+            (\(a,b) -> sformat ("hello "%int%" world "%int) a b)
+      , bench "show" $ simpleBench
+            (\(a,b) -> "hello " <> tshow a <> " world " <> tshow b)
+      , bench "text-format" $ simpleBench
+            (format' "hello {} world {}")
+      , bench "printf" $ simpleBench
+            (\(a,b) -> T.pack $ printf "hello %d world %d" a b)
+      , bench "interpolate" $ simpleBench
+            (\(a,b) -> T.pack [i|hello #{a} world #{b}|])
       ]
     ]
   ]
