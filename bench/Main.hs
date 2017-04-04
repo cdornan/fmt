13d0969fd43c68236ba313740b8d4740f7837ff7
Dmitry Kovanikov <kovanikov@gmail.com>
2017-04-04 21:23:22 +0300

    Add String simple benchmarks

--- bench/Main.hs
+++ bench/Main.hs
@@ -6,7 +6,9 @@
 
 module Main where
 
+import           Control.DeepSeq         (NFData)
 import           Data.Monoid             ((<>))
+import           Data.String             (IsString)
 import           Data.String.Interpolate (i)
 import           Data.Text               (Text)
 import qualified Data.Text               as T
@@ -14,10 +16,10 @@ import qualified Data.Text.Format        as TF
 import           Data.Text.Format.Params as TF
 import qualified Data.Text.Lazy          as LT
 import           Fmt                     (( #| ), (|#))
-import           Formatting              (int, sformat, (%))
+import           Formatting              (formatToString, int, sformat, (%))
 import           Text.Printf             (printf)
 
-import           Criterion               (Benchmark, Benchmarkable, bench, bgroup, nf)
+import           Criterion               (Benchmark, bench, bgroup, nf)
 import           Criterion.Main          (defaultMain)
 
 ----------------------------------------------------------------------------
@@ -27,6 +29,9 @@ import           Criterion.Main          (defaultMain)
 format' :: TF.Params ps => TF.Format -> ps -> Text
 format' f = LT.toStrict . TF.format f
 
+formatS :: TF.Params ps => TF.Format -> ps -> String
+formatS f = LT.unpack . TF.format f
+
 tshow :: Show a => a -> Text
 tshow x = T.pack (show x)
 
@@ -34,11 +39,17 @@ tshow x = T.pack (show x)
 -- Benchmarks utility functions
 ----------------------------------------------------------------------------
 
-simpleBench :: ((Int, Int) -> Text) -> Benchmarkable
-simpleBench fmt = nf fmt (1,2)
+bGenericStringGroup :: (NFData s, IsString s) => String -> a -> [(String, a -> s)] -> Benchmark
+bGenericStringGroup sTag benchObj = bgroup sTag . map (\(tag, fmt) -> bench tag (nf fmt benchObj))
+{-# INLINABLE bGenericStringGroup #-}
 
 bTextGroup :: a -> [(String, a -> Text)] -> Benchmark
-bTextGroup benchObj = bgroup "text" . map (\(tag, fmt) -> bench tag (nf fmt benchObj))
+bTextGroup = bGenericStringGroup "text"
+{-# INLINE bTextGroup #-}
+
+bStringGroup :: a -> [(String, a -> String)] -> Benchmark
+bStringGroup = bGenericStringGroup "string"
+{-# INLINE bStringGroup #-}
 
 -- Function for convenience instead of using manual @(,)@.
 taggedB :: String -> (a -> b) -> (String, a -> b)
@@ -47,11 +58,7 @@ taggedB = (,)
 ----------------------------------------------------------------------------
 -- Benchmakrs themselves
 ----------------------------------------------------------------------------
-{-
-bTextGroup
-[ ("fmt", \(a,b) -> "")
-]
--}
+
 main :: IO ()
 main = defaultMain
   [ bgroup "simple"
@@ -62,6 +69,14 @@ main = defaultMain
       , taggedB "interpolate" $ \(a,b) -> T.pack [i|hello #{a} world #{b}|]
       , taggedB "show"        $ \(a,b) -> "hello " <> tshow a <> " world " <> tshow b
       , taggedB "printf"      $ \(a,b) -> T.pack $ printf "hello %d world %d" a b
+      ]
+    , bStringGroup (1 :: Int, 2 :: Int)
+      [ taggedB "fmt"         $ \(a,b) -> "hello "#|a|#" world "#|b|#""
+      , taggedB "formattting" $ \(a,b) -> formatToString ("hello "%int%" world "%int) a b
+      , taggedB "text-format" $ formatS "hello {} world {}"
+      , taggedB "interpolate" $ \(a,b) -> [i|hello #{a} world #{b}|]
+      , taggedB "show"        $ \(a,b) -> "hello " ++ show a ++ " world " ++ show b
+      , taggedB "printf"      $ \(a,b) -> printf "hello %d world %d" a b
       ]
     ]
   ]
